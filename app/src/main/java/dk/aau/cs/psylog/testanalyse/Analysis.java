8a5fa4a8fc9099bf0055535cb402fb627fafe051
Mathias Winde <Mapede11@student.aau.dk>
2015-03-10 11:30:12 +0100

    Har ændret den til at ligne sensorer mere :)

---
+++ app/src/main/java/dk/aau/cs/psylog/testanalyse/Analysis.java
@@ -0,0 +1,100 @@
+package dk.aau.cs.psylog.testanalyse;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Analysis {
+
+    private ContentResolver resolver;
+    private Uri read;
+    private Uri write;
+
+    private Timer timer;
+    private TimerTask timerTask;
+    private long delay;
+    private long period;
+
+    public Analysis(Context context)
+    {
+        resolver = context.getApplicationContext().getContentResolver();
+        String uriBase = "content://dk.aau.cs.psylog.psylog";
+        read = Uri.parse(uriBase + "/illuminance");
+        write = Uri.parse(uriBase + "/testAnalyse");
+
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                String[] projection = {"lux"};
+                Cursor cursor = resolver.query(read, projection , null, null, null);
+                float report = analyse(cursor);
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("testdata", report);
+                resolver.insert(write, contentValues);
+                Log.i("TestAnalyse", "avg light readings " + report + " lux");
+                String[] projection2 = {"testdata"};
+                Cursor cursor2 = resolver.query(write, projection2, null, null, null);
+                if (cursor2.moveToFirst())
+                {
+                    Log.i("test", "starting reads");
+                    do {
+                        float result = cursor2.getFloat(0);
+                        Log.i("test", "Read from analysis module: " + result);
+                    } while (cursor2.moveToNext());
+                }
+            }
+        };
+    }
+
+    public float analyse(Cursor cursor)
+    {
+        List<Float> content = new ArrayList<Float>();
+        if (cursor.moveToFirst())
+        {
+            do {
+                float result = cursor.getFloat(0);
+                content.add(result);
+            } while (cursor.moveToNext());
+        }
+
+        if (!content.isEmpty())
+        {
+            float sum = 0;
+            int j = 0;
+            for (; j < content.size(); j++)
+            {
+                sum += content.get(j);
+            }
+            float output = sum / (float)j;
+            return output;
+        }
+        return 0;
+    }
+
+    public void startAnalysis(){
+        if (timer == null) {
+            timer = new Timer();
+        }
+        // skal muligvis stoppes inden reschedule
+        timer.schedule(timerTask, delay, period);
+    }
+
+    public void stopAnalysis(){
+        timer.cancel();
+        timer.purge();
+    }
+
+    public void analysisParameters(Intent intent) {
+        period = intent.getIntExtra("period",1000);
+        delay = intent.getIntExtra("delay",0);
+    }
+}
--- app/src/main/java/dk/aau/cs/psylog/testanalyse/PsyLogService.java
+++ app/src/main/java/dk/aau/cs/psylog/testanalyse/PsyLogService.java
@@ -1,22 +1,13 @@
 package dk.aau.cs.psylog.testanalyse;
 
 import android.app.Service;
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.IBinder;
-import android.util.Log;
-
-import java.util.ArrayList;
-import java.util.List;
 
 // One time tasks should be IntentService?
 public class PsyLogService extends Service {
 
-    ContentResolver resolver;
-
+    Analysis analysis;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -27,71 +18,20 @@ public class PsyLogService extends Service {
     public void onCreate()
     {
         super.onCreate();
+        analysis = new Analysis(this);
     }
 
 
     @Override
     public int onStartCommand(Intent intent, int flag, int startid)
     {
-        analysisParameters(intent);
-        testAnalysis();
+        analysis.analysisParameters(intent);
+        analysis.startAnalysis();
         return Service.START_NOT_STICKY;
     }
 
-    private void testAnalysis()
-    {
-        resolver = getApplicationContext().getContentResolver();
-        String uriBase = "content://dk.aau.cs.psylog.psylog";
-        Uri read = Uri.parse(uriBase + "/illuminance");
-        Uri write = Uri.parse(uriBase + "/testAnalyse");
-
-        String[] projection = {"lux"};
-        Cursor cursor = resolver.query(read, projection , null, null, null);
-        float report = analyse(cursor);
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("testdata", report);
-        resolver.insert(write, contentValues);
-        Log.i("TestAnalyse", "avg light readings " + report + " lux");
-        String[] projection2 = {"testdata"};
-        Cursor cursor2 = resolver.query(write, projection2, null, null, null);
-        if (cursor2.moveToFirst())
-        {
-            Log.i("test", "starting reads");
-            do {
-                float result = cursor2.getFloat(0);
-                Log.i("test", "Read from analysis module: " + result);
-            } while (cursor2.moveToNext());
-        }
-    }
-
-    public float analyse(Cursor cursor)
-    {
-        //android.os.Debug.waitForDebugger();
-        List<Float> content = new ArrayList<>();
-        if (cursor.moveToFirst())
-        {
-            do {
-                float result = cursor.getFloat(0);
-                content.add(result);
-            } while (cursor.moveToNext());
-        }
-
-        if (!content.isEmpty())
-        {
-            float sum = 0;
-            int j = 0;
-            for (; j < content.size(); j++)
-            {
-                sum += content.get(j);
-            }
-            float output = sum / (float)j;
-            return output;
-        }
-        return 0;
-    }
-
-    public void analysisParameters(Intent intent)
-    {
-
+    @Override
+    public void onDestroy(){
+        analysis.stopAnalysis();
     }
 }
